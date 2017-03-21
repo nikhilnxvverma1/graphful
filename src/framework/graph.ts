a30c1449247d9983ef7d3c474d4669d7b294a0dc
Nikhil Verma <nikhilnxvverma1@aol.com>
2017-03-20 22:03:48 -0400

    Methods for node retrieval in the graph

--- src/framework/graph.ts
+++ src/framework/graph.ts
@@ -5,5 +5,36 @@ export class GFGraph{
 	nodeList:GFNode[];
 	edgeList:GFEdge[];
 
-	//TODO methods for filtering nodes based on a selector
+	//methods for filtering nodes based on a selector
+
+	getNodeById(id:string):GFNode{
+		for(let i=0;i<this.nodeList.length;i++){
+			if(this.nodeList[i].id==id){
+				return this.nodeList[i];
+			}
+		}
+		return null;
+	}
+
+	getNodesOfType(type:string):GFNode[]{
+		let filteredNodes:GFNode[]=[];
+		for(let i=0;i<this.nodeList.length;i++){
+			if(this.nodeList[i].type==type){
+				filteredNodes.push(this.nodeList[i]);
+			}
+		}
+		return filteredNodes;
+	}
+
+	/** Returns nodes in a path in the object graph. TODO Not ready yet */
+	getNodeInPath(path:string):GFNode{
+		let pathItems:string[]=path.split(".");
+		let rootNode=this.getNodeById(pathItems[0]);
+		let parent=rootNode;
+		let leaf=rootNode;
+		for(let i=0;i<pathItems.length;i++){
+			//TODO do this recursively
+		}
+		return leaf;
+	}
 }
--- src/framework/node.ts
+++ src/framework/node.ts
@@ -3,10 +3,18 @@ import { GFEdge } from './edge';
 export class GFNode{
 	id:string;
 	type:string;
-	tags:string[];
 	content:string[];
 	parent:GFNode;
-	children:GFNode[];
 	edgeList:GFEdge[];
 	private code;
+
+	getConnectedNodes(edgeName:string):GFNode[]{
+		let connectedNodes:GFNode[]=[];
+		for(let i=0;i<this.edgeList.length;i++){
+			if(this.edgeList[i].name==name){
+				connectedNodes.push(this.edgeList[i].node2);
+			}
+		}
+		return connectedNodes;
+	}
 }
