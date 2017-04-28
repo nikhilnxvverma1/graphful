48e820cf72235f5a47a054e268f4cc8462e62ba0
Nikhil Verma <nikhilnxvverma1@aol.com>
2017-04-28 08:48:31 -0400

    Test case for context free grammer generation

--- src/tests/syntax-parser.spec.ts
+++ src/tests/syntax-parser.spec.ts
@@ -1,4 +1,5 @@
 import { NonTerminal,Terminal,SyntaxElementType } from 'framework/compiler/syntax-parser';
+import { Rule,ContextFreeGrammer } from 'framework/compiler/syntax-parser';
 import { LexemeType } from 'framework/compiler/lexical-analyzer';
 
 describe('Non Terminal', () => {
@@ -31,4 +32,74 @@ describe('Terminal', () => {
 		expect(terminal.isEndOfFile()).toBeFalsy();
 	});
 
+});
+
+describe('ContextFreeGrammer Generation', () => {
+	let cfg:ContextFreeGrammer;
+	let expectedNonTerminals;
+	let expectedTerminals;
+	let expectedRules;
+
+	beforeEach(() => {
+		const ruleList = [
+			"Expr -> Expr Op Expr | $4",
+			"Op -> $6 | $7"
+		];
+		cfg=ContextFreeGrammer.grammerFrom(ruleList);
+		expectedNonTerminals=2+1;//1 for the augumented rule
+		expectedTerminals=3;
+		expectedRules=4+1;//1 for augumented rule
+	});
+
+	it('should start with an augumented rule',()=>{
+		expect(cfg.start.isAugumentedVariable()).toBeTruthy();
+	});
+
+	it('should contain all the non terminals',()=>{
+		expect(cfg.variableList.length).toBe(expectedNonTerminals);
+	});
+
+	it('should contain all the terminals',()=>{
+		expect(cfg.variableList.length).toBe(expectedTerminals);
+	});
+
+	it('should contain all the rules',()=>{
+		expect(cfg.relation.length).toBe(expectedRules);
+	});
+
+	it('should have correct LHS in all the rules',()=>{
+		expect(cfg.relation[0].lhs.isAugumentedVariable()).toBeTruthy();
+		expect(cfg.relation[1].lhs.representation).toEqual("Expr");
+		expect(cfg.relation[2].lhs.representation).toEqual("Expr");
+		expect(cfg.relation[3].lhs.representation).toEqual("Op");
+		expect(cfg.relation[4].lhs.representation).toEqual("Op");
+	});
+
+	it('should have correct RHS in all the rules',()=>{
+		//augumented
+		expect(cfg.relation[0].rhs[0].getType()).toBe(SyntaxElementType.NonTerminal);
+		expect((cfg.relation[0].rhs[0] as NonTerminal).representation).toBe("Expr");
+
+		//Expr -> Expr Op Expr
+		expect(cfg.relation[1].rhs[0].getType()).toBe(SyntaxElementType.NonTerminal);
+		expect((cfg.relation[1].rhs[0] as NonTerminal).representation).toBe("Expr");
+		expect(cfg.relation[1].rhs[1].getType()).toBe(SyntaxElementType.NonTerminal);
+		expect((cfg.relation[1].rhs[1] as NonTerminal).representation).toBe("Op");
+		expect(cfg.relation[1].rhs[2].getType()).toBe(SyntaxElementType.NonTerminal);
+		expect((cfg.relation[1].rhs[2] as NonTerminal).representation).toBe("Expr");
+
+		//Expr -> $4
+		expect(cfg.relation[2].rhs[0].getType()).toBe(SyntaxElementType.Terminal);
+		expect((cfg.relation[2].rhs[0] as Terminal).token).toBe(4);
+		
+		//Op -> $6
+		expect(cfg.relation[3].rhs[0].getType()).toBe(SyntaxElementType.Terminal);
+		expect((cfg.relation[3].rhs[0] as Terminal).token).toBe(6);
+		
+		//Op -> $7
+		expect(cfg.relation[4].rhs[0].getType()).toBe(SyntaxElementType.Terminal);
+		expect((cfg.relation[4].rhs[0] as Terminal).token).toBe(7);
+		
+		
+	});
 });
